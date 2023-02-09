20bf58594fc8c671c9d4da3ea921ed91f18e6411
John Waczak <john.louis.waczak@gmail.com>
2023-02-09 10:29:02 -0600

    added r2 back

--- src/recipes.jl
+++ src/recipes.jl
@@ -6,6 +6,18 @@ using StatsPlots: density, qqplot
 import KernelDensity
 
 
+"""
+Compute the r² value between two vectors.
+"""
+function r²(y_pred, y_true)
+    ȳ = mean(y_true)
+    SS_res = sum([(y_true[i]-y_pred[i])^2 for i ∈ 1:length(y_pred)])
+    SS_tot = sum([(y_true[i]-ȳ)^2 for i ∈ 1:length(y_pred)])
+    return 1 - SS_res/(SS_tot + eps(eltype(y_pred)))
+end
+
+
+
 
 @userplot HistPDF
 
